4bd29ec474c3c13a481aa76aa928f1b4a1cac089
pascal.jego <pascal.jego@outlook.com>
2019-07-06 21:31:46 +0200

    initial commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Customer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Invoice;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        $chrono = 1;
+        
+        for ($c = 0; $c < 30; $c++) {      
+            $customer = new Customer();
+            $customer->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email);
+                    
+            $manager->persist($customer);
+
+            for ($i = 0; $i < mt_rand(3, 10); $i++) { 
+                $invoice = new Invoice();
+                $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+                        ->setSentAt($faker->dateTimeBetween('-6 months'))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+                        ->setCustomer($customer)
+                        ->setChrono($chrono)
+                        ;
+
+                $chrono++;
+
+                $manager->persist($invoice);
+            }
+        }
+        
+        $manager->flush();
+    }
+}
