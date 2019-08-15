68639f23616c5be24181e340550e43fd022449f2
pascal.jego <pascal.jego@outlook.com>
2019-08-15 11:15:07 +0200

    Installation de l'API platform et mise en place de nos ressources

---
+++ src/Controller/InvoiceIncrementationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class InvoiceIncrementationController {    
+
+    /**
+     * Undocumented variable
+     *
+     * @var ObjectManager
+     */
+    private $manager;
+
+    public function __construct(ObjectManager $manager)
+    {
+           $this->manager = $manager;
+    }
+
+    public function __invoke(Invoice $data)
+    {
+        $data->setChrono($data->getChrono() + 1);
+
+        $this->manager->flush();
+
+        return $data;
+    }
+    
+}
