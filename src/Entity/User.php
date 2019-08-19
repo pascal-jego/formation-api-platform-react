99e17657a11a17a53a869af4f7a4be071510b552
pascal.jego <pascal.jego@outlook.com>
2019-08-19 15:19:07 +0200

    Ajout de l'authentification via JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,9 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
+ * @ApiResource(
+ *  normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity("email",message="Un utilisateur ayant cette adresse email existe déjà")
  */
 class User implements UserInterface
@@ -22,13 +24,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"customers_read", "invoices_read","invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read","invoices_subresource","users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_read", "invoices_read","invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="L'email doit être renseigné !")
      * @Assert\Email(message="L'adresse email doit avoir un format valide !")
      */
@@ -36,7 +38,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="json")
-     * @Groups({"customers_read"})
+     * @Groups({"customers_read","users_read"})
      */
     private $roles = [];
 
@@ -49,7 +51,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read","invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le prénom est obligatoire")
      * @Assert\Length(
      *  min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères", 
@@ -59,7 +61,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_read", "invoices_read","invoices_subresource"})
+     * @Groups({"customers_read", "invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le prénom est obligatoire")
      * @Assert\Length(min=3, minMessage="Le nom doit faire entre 3 et 255 caractères",max=255, maxMessage="Le nom doit faire 255 caractères maximum")
      */
